13af4bbb3764a3ecb225b1191ea4f5f565e9b76e
dhanendra46 <121929777+dhanendra46@users.noreply.github.com>
2023-12-14 19:07:50 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -7,9 +7,9 @@ const config = {
   //retries : 1,
   workers: 1,
   /* Maximum time one test can run for. */
-  timeout: 90 * 1000,
+  timeout: 30 * 1000,
   expect: {
-    timeout: 90000
+    timeout: 30000
   },
 
   use: {

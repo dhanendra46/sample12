eb831d3c59ab116ae8428a1545ab72b865b01eee
dhanendra46 <dhanendra.nimmakayala@optimworks.com>
2024-01-09 13:24:12 +0530

    slack integration

--- tests/slack.spec.js
+++ tests/slack.spec.js
@@ -3,19 +3,9 @@
 const { test } = require('@playwright/test');
 
 test('Example test 1', async ({ page }) => {
-  // Your test logic here
-  //await page.goto('https://www.example.com');
-  await page.waitForSelector('h1');
-});
-
-test('Example test 2', async ({ page }) => {
-  // Your test logic here
-  await page.goto('https://www.example.com');
-  await page.waitForSelector('.nonexistent-element', { timeout: 5000 });
-});
-
-test('Example test 3', async ({ page }) => {
-  // Your test logic here
-  await page.goto('https://www.example.com');
-  await page.waitForSelector('h2');
+    const specname = "example 1"
+    await page.goto("https://www.youtube.com/")
+    await page.click(".container")
+    console.log(`::set-output name=failed_spec::${page.testInfo.title}`)
+    throw new Error("Your error message ['"+specname+'"]')
 });
